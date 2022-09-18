a1de2b9feaf9b534d919ffaceb6cfae5717e9713
Daniil-W <vorona.daniil.work@gmail.com>
2022-09-19 00:17:17 +0300

    first commit

---
+++ test_iLEVEL/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Menu></Menu>
+</template>
+
+
+<script>
+import Menu from "./components/Menu.vue";
+export default {
+  components:{
+    Menu
+  },
+}
+</script>
+
+
+<style scoped>
+</style>
